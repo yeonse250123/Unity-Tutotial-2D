29882291229da410b7aee7e00eb47ca6819344dc
yeonse250123 <yeonse250123@gmail.com>
2025-06-05 10:53:42 +0900

    Cat InputField

--- Assets/02. Scripts/Cat/UIManager.cs
+++ Assets/02. Scripts/Cat/UIManager.cs
@@ -6,12 +6,35 @@ namespace Cat
 {
     public class UIManager : MonoBehaviour
     {
+        public GameObject playObj;
+        public GameObject introUI;
+
         public TMP_InputField inputField;
         public TextMeshProUGUI nameTextUI;
 
+        public Button startButton;
+
+        private void Start()
+        {
+            startButton.onClick.AddListener(OnStartButton);
+        }
+
         public void OnStartButton()
         {
-            nameTextUI.text = inputField.text;
+            bool isNoText = inputField.text == "";
+
+            if (isNoText)
+            {
+                Debug.Log("입력한 텍스트 없음");
+            }
+            else
+            {
+                playObj.SetActive(true);
+                introUI.SetActive(false);
+
+                Debug.Log($"{nameTextUI} 입력");
+                nameTextUI.text = inputField.text;
+            }
         }
     }
 }
