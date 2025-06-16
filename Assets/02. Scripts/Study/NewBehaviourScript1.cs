7ff82abec855f79a8069dc42b0c1f16c4780e07e
yeonse250123 <yeonse250123@gmail.com>
2025-06-16 16:11:30 +0900

    .

---
+++ Assets/02. Scripts/Study/NewBehaviourScript1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
